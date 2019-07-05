976ac518c31605200d4be13b675059e5cd4a6788
EkoGroszek <jachimczak3125@gmail.com>
2019-07-05 12:21:20 +0200

    elo shit

--- demo/src/main/java/com/example/demo/services/TransferServiceImpl.java
+++ demo/src/main/java/com/example/demo/services/TransferServiceImpl.java
@@ -19,6 +19,8 @@ public class TransferServiceImpl {
 //    private AccountRepository accountRepository;
     private AccountServiceImpl accountService;
 
+    private int elop;
+
     public TransferServiceImpl(){
 
     }
